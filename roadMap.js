5be27aed79eb42dd607c68cb798182aab47c5a3f
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-11-12 23:27:34 +0700

    Buổi 1

---
+++ roadMap.js
@@ -0,0 +1,83 @@
+const roadMap = {
+    MySQL : {
+        "Buổi 1": "Khải giảng",
+        "Buổi 2": "MySQL Trong Docker, Basic CRUD Database, Table",
+        "Buổi 3": "Query SQL, Relationship"
+    },
+    ExpressJS: {
+        "Buổi 4": [
+            "- Khởi tạo source với ExpressJS",
+            "- Áp dụng mô hình MVC"
+        ],
+        "Buổi 5": [
+            "- Middleware",
+            "- Áp Middleware vào xử lý lỗi"
+        ],
+        "Buổi 6": [
+            "- ORM sequelize",
+            "- Database First, Code First",
+            "- ORM prisma",
+            "- Biến môi trường"
+        ],
+        "Buổi 7": [
+            "- Phân trang, Filter",
+            "- Ráp FE",
+            "- CORS"
+        ]
+    },
+    Auth: {
+        "Buổi 8": [
+            "- Xây dựng chức năng Đăng ký, Đăng nhập",
+            "- AcccessToken (json web token)",
+            "- Gửi Email"
+        ],
+        "Buổi 9": [
+            "- Middleware bảo vệ API",
+            "- Login Goole"
+        ],
+        "Buổi 10": [
+            "- Demo hack facebook",
+            "- Refresh Token (json web token)"
+        ],
+        "Buổi 11": [
+            "- Upload Hình Local",
+            "- Upload Hình Cloud",
+        ],
+        "Buổi 12": [
+            "- Swagger API",
+            "- GraphQL"
+        ]
+    },
+    Prisma: {
+        "Buổi 13": [
+            "- Socket IO",
+            "- Xây dựng chức năng chat Một Một Và Chat Nhóm"
+        ],
+        "Buổi 14": [
+            "- Khởi tạo source NestJS",
+            "- Cấu trúc & Giao tiếp giữa các module"
+        ],
+        "Buổi 15": [
+            "- Microservice"
+        ],
+    },
+    NestJS: {
+        "Buổi 16": [
+            "- Cache memory",
+            "- Cache Redis"
+        ],
+        "Buổi 17":[
+            "- Elesticsearch",
+            "- Kibana"
+        ]
+    },
+    Docker: {
+        "Buổi 18": [
+            "- Tìm hiểu về docker",
+            "- Tìm hiểu về docker-compose"
+        ],
+        "Buổi 19": [
+            "- Deploy CI/CD lên server"
+        ]
+    }
+}
